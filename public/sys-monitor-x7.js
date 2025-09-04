30e97817d77d4917c7a5898badec807fa6e6ee84
negociosdigitaisesp <negociosdigitaisesp@gmail.com>
2025-09-03 23:29:33 -0300

    Create sys-monitor-x7.js

---
+++ public/sys-monitor-x7.js
@@ -0,0 +1,8 @@
+export default function SysMonitorX7() {
+  return (
+    <div>
+      <h1>Sys Monitor X7 funcionando 🚀</h1>
+      <p>Aqui você pode editar o conteúdo em React/JSX.</p>
+    </div>
+  );
+}
